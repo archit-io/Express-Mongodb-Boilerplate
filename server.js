e5e5fabc584ab9db7a91bcf84ff6065792316586
Archit Choudhary <archit.digisocial@gmail.com>
2022-06-16 16:33:58 +0530

    added express method app.post to handle post requests

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ app.get('/', (req, res) => {
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   })
+
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })
