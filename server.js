f3a9a6dbbc72e84f4059972a8d82e4a555b018ba
Archit Choudhary <archit.digisocial@gmail.com>
2022-06-16 17:45:36 +0530

    added the method to create data in the db

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const connectionString = `mongodb+srv://archit:${process.env.DB_PASSWORD}@cluster0.mrxqhgv.mongodb.net/?retryWrites=true&w=majority`
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
 .then(client => {
   console.log('Connected to Database')
   const db = client.db('Cluster0')
+  const quotesCollection = db.collection('quotes')
+
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => console.error(error))
+  })
 })
 
-// Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -33,6 +44,6 @@ app.get('/', (req, res) => {
 //     console.log('Hellooooooooooooooooo!')
 //   })
 
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
+// app.post('/quotes', (req, res) => {
+//   console.log(req.body)
+// })
